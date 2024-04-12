6cfd07796c6b0c2786d316633ccdafb73c9cd8ec
Adam Hassan <146145364+Adimate132@users.noreply.github.com>
2024-04-12 11:26:41 -0500

    updated image generator project
    - added image grab from unsplash api

--- Portfolio/projects/ReferenceGenerator/refgen.js
+++ Portfolio/projects/ReferenceGenerator/refgen.js
@@ -10,29 +10,35 @@ window.addEventListener('load', function() {
 
 
 const startForm = document.getElementById('form-box');
-
+const imageGrid = document.getElementById('generated-images')
 
 startForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const startFormData = new FormData(startForm);
-    const input = startFormData.get('form-box').trim().toLowerCase()
+    const input = startFormData.get('form-box').toLowerCase()
 
-    // api call test
-
-    fetchData();
-    async function fetchData() {
+    fetchPhotos();
+    async function fetchPhotos() {
         try {
-            const response = await  fetch(`https://pokeapi.co/api/v2/pokemon/${input}`, {method: 'GET'});
-            if (!response.ok)
+            const response = await fetch(`https://api.unsplash.com/search/photos/?query=${input}&client_id=RdaSuzcBBK6eJUeM_4r1jUDCZtrhLdkGAB8vtLW8XhI`)
+            if (!response.ok) 
                 throw new Error("Could not fetch resource")
-
-                const data = await response.json();
-                alert(`${data.name} is a pokemon :D`)
+            
+            const data = await response.json()
+            data.results.forEach(item => {
+                let newImg = document.createElement('img')
+                newImg.src = `${item.urls.regular}`
+                appendToGrid(newImg) // append to grid
+            })
         }
-        catch(error) {
-            console.log(error)
-            alert(`${input} is not a pokemon :(`)
+        catch (error) {
+            console.log('Error: ' + error)
         }
     }
+
 })
+
+function appendToGrid(image) {
+    imageGrid.appendChild(image)
+}
