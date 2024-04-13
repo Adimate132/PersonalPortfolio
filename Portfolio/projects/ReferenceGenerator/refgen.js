b7284adf1d12b48cd630a7abddbd8a72f46a164b
Adam Hassan <146145364+Adimate132@users.noreply.github.com>
2024-04-13 01:35:14 -0500

    updated ref img project

--- Portfolio/projects/ReferenceGenerator/refgen.js
+++ Portfolio/projects/ReferenceGenerator/refgen.js
@@ -18,19 +18,29 @@ startForm.addEventListener('submit', function(e) {
     const startFormData = new FormData(startForm);
     const input = startFormData.get('form-box').toLowerCase()
 
-    fetchPhotos();
-    async function fetchPhotos() {
+    fetchPhotos(input);
+    async function fetchPhotos(input) {
         try {
             const response = await fetch(`https://api.unsplash.com/search/photos/?query=${input}&client_id=RdaSuzcBBK6eJUeM_4r1jUDCZtrhLdkGAB8vtLW8XhI`)
             if (!response.ok) 
                 throw new Error("Could not fetch resource")
-            
+            resetGrid() // clear grid 
+
             const data = await response.json()
-            data.results.forEach(item => {
+            let randomPage = Math.floor(Math.random() * data.total_pages)
+            if (randomPage == 0)
+                randomPage += 1
+
+            
+            const secondResponse = await fetch(`https://api.unsplash.com/search/photos/?query=${input}&client_id=RdaSuzcBBK6eJUeM_4r1jUDCZtrhLdkGAB8vtLW8XhI&page=${randomPage}`)
+            const newData = await secondResponse.json()
+            
+            newData.results.forEach(item => {
                 let newImg = document.createElement('img')
                 newImg.src = `${item.urls.regular}`
                 appendToGrid(newImg) // append to grid
             })
+            
         }
         catch (error) {
             console.log('Error: ' + error)
@@ -41,4 +51,7 @@ startForm.addEventListener('submit', function(e) {
 
 function appendToGrid(image) {
     imageGrid.appendChild(image)
+}
+function resetGrid() {
+    imageGrid.innerHTML = ""
 }
